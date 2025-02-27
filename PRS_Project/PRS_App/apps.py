378584fcf238d253373b20ca908e8a9f0c3c13c3
samrussell-code <sc22s3r@leeds.ac.uk>
2025-02-27 16:34:09 +0000

    init

---
+++ PRS_Project/PRS_App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PrsAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "PRS_App"
---
+++ tst.py
@@ -0,0 +1,4 @@
+import django
+print(django.VERSION)
+# django-admin startproject PRS_Project
+# python manage.py startapp PRS_App
